076debc23299a7724e6a1b66306f639f0304f944
Administrator <Administrator@192.168.222.1>
2017-04-22 09:39:47 -0500

    updated

--- YahooProject/src/test/java/com/Login/test1.java
+++ YahooProject/src/test/java/com/Login/test1.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class test1 {
 
 	public static void main(String[] args) {
- 
+ //this is test file
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		
