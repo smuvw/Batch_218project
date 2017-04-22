8355670da30fe1fca6ec0b20442ff42044acb6db
Administrator <Administrator@192.168.222.1>
2017-04-22 09:37:18 -0500

    moving yhoo file

---
+++ YahooProject/src/test/java/com/Login/test1.java
@@ -0,0 +1,15 @@
+package com.Login;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test1 {
+
+	public static void main(String[] args) {
+ 
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+
+	}
+
+}
